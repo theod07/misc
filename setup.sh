dddb0b472bdabf7042fbe0b9d601900c166c695e
theod07 <theodore.do@gmail.com>
2016-12-22 10:45:08 -0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -99,6 +99,104 @@ brew install mackup
 #brew install go
 #brew install speedtest_cli
 #brew install aws-shell
+
+###################################################
+###################################################
+#`nmap`- an outstanding network management package
+#`nodejs` - the node runtime which supports server-side JavaScript
+#`multi markdown` - supports the awesome mmd formatting system
+#`emacs` (or vim) pick your poison (+ispell if you use emacs)
+#`bash` - gives you v4, a more modern version (note you need to add it to approved shells and use chsh to switch to it
+#`bash-completion` - excellent escape command completion for bash
+#`git-crypt` - to support automatic encryption on pushing to git
+#`speedtest_cli` - gives you network bandwidth measurement
+#`aws-shell` - if you have an AWS account
+#`wget `- for simple high speed file transfer
+#awscli - commandline tools for Amazon web services -- frequently updated. Nice to have brew take care of it
+#bash-completion - smarter autocomplete for bash
+#calc - my go-to calculator
+#ccrypt - strong file crypt tool
+#dos2unix - convert CRLF <--> CR
+#geoip - find where an IP address comes from
+#git-flow
+#hh - shell history browser / searcher
+#htop - best top replacement
+#irssi - my favorite commandline irc client
+#jq - 'like sed for JSON data'
+#lftp - nice commandline ftp
+#links - best text mode web browser
+#lynx - second best text mode web browser
+#ncdu - ncurses disk usage browser
+#nmap - never know when you will need to port scan something
+#tig - curses git repo browser
+#tmux - terminal multiplexer - a better 'screen'
+#tree - view directory structure under CWD
+#unrar - unarchive .rar files
+#wget - grab remote files with a URL
+#vimpager - use vim colorization rules for paging through any file - you may want to replace 'less' with this
+#wget
+#git
+#cask
+#macvim (uses cask to install)
+#imagemagick
+#irssi
+#wget
+#ffmpeg
+#htop
+#imagemagick
+#mysql
+#Homebrew cask
+#ack - ack is a tool like grep, optimized for programmers
+#git
+#bash-completion - bash completion script
+#colordiff
+#coreutils -I prefer GNU core utils, such as: ls, ln, mv, id, df, cut, echo etc...
+#gawk 
+#gnu-sed
+#cowsay
+#lftp
+#wget
+#axel
+#tree
+#unrar
+#rlwrap
+#readline wrap
+#tig - git command line interface
+#brew cask install caffeine
+#brew cask install dropbox
+#brew cask install evernote
+#brew cask install firefox
+#brew cask install google-chrome
+#brew cask install google-drive
+#brew cask install iterm2
+#brew cask install kindle
+#brew cask install omnigraffle
+#brew cask install skype
+#brew cask install slack
+#brew cask install spectacle
+#brew cask install spotify
+#brew cask install sublime
+#brew cask install the-unarchiver
+#brew cask install utorrent
+#brew cask install virtualbox
+#brew install macvim
+#brew install dockutil
+#brew cask install intellij-idea
+#brew install gpg
+#brew cask install java
+#brew install jq
+#I use RVM: Ruby Version Manager for ruby versions, creationix/nvm for node versions and SDKMAN! the Software Development Kit Manager for JVM based languages/tools.
+#sdk install gradle
+#sdk install groovy
+#sdk install scala
+#sdk install grails
+#sdk install springboot
+#sdk install maven
+#sdk install sbt
+#on a fresh mac I always run dockutil --remove all to cleanup the dock bar
+#to be continued.. https://www.quora.com/What-are-the-first-or-must-have-homebrew-packages-that-you-install-on-your-Mac
+###################################################
+###################################################
  
 cd /Applications
 wget https://justgetflux.com/mac/Flux.zip
