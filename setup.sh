8414266baf6fef54cdd062d16d072abaf34bcced
theod07 <theodore.do@gmail.com>
2018-05-22 10:41:27 -0700

    remove android-studio

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ brew cask install pycharm-ce
 brew cask install openshot-video-editor
 brew cask install screaming-frog-seo-spider
 brew cask install limechat
-brew cask install android-studio
+# brew cask install android-studio
 brew cask install mactex
 brew cask install pdftotext
 brew cask install keycastr
